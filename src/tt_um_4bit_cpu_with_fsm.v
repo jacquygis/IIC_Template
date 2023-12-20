b3f5b5fc9ef21dc40e0ec192cdb079f05996bded
jacquygis <jacqueline.gislai@gmx.at>
2023-12-20 09:28:42 +0100

    errors fixed

--- src/tt_um_4bit_cpu_with_fsm.v
+++ src/tt_um_4bit_cpu_with_fsm.v
@@ -1,49 +1,39 @@
 `default_nettype none
 
 //converting the 8-Bit-Input to a 4-bit-input-signal via MSB
-module EightToFourBitConverter( 
+/*module EightToFourBitConverter( 
 	input [7:0] eightBitInput,
-	output reg [3:0] fourBitOutput,
+	output [3:0] fourBitOutput,
 	input clk,
 	input rst
 );
 
 always @(posedge clk or posedge rst) begin
-	if (rst) begin
-		//Reset: all output bits on 0
-		fourBitOutput <= 4'b0000;
-	end else begin
-		//MSB put in 4-Bit-Output
-		fourBitOutput <= eightBitInput[7:4];
-	end
+	//MSB put in 4-Bit-Output
+	fourBitOutput <= eightBitInput[7:4];
 end
 endmodule
 
 //converting the 4-bit-output-signal to a 8-bit-output 
 module FourToEightBitConverter(
-	input [3:0] fourBitInput,
+	input reg [3:0] fourBitInput,
 	output reg [7:0] eightBitOutput,
 	input clk,
 	input rst
 );
 
 always @(posedge clk or posedge rst) begin
-	if (rst) begin
-		//Reset: all output bits on 0
-		eightBitOutput <= 8'b00000000;
-	end else begin
-		//4-bit-input put on MSB of 8-bit-output
-		eightBitOutput <= {fourBitInput, 4'b0000};
-	end
+	//4-bit-input put on MSB of 8-bit-output
+	eightBitOutput <= {fourBitInput, 4'b0000};
 end
-endmodule
+endmodule*/
 
 
 module tt_um_4bit_cpu_with_fsm (
     input  wire [7:0] in_data_eightBit,  // input data
     input  wire [7:0] in_addr_eightBit,  // storage addresses
     input  wire [7:0] in_opcode_eightBit, //opcode for chosing operations
-    output wire [7:0] out_data_eightBit, // output data
+    output reg [7:0] out_data_eightBit, // output data
     input  wire       write_ena,// high when writing for storage is active
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -51,31 +41,41 @@ module tt_um_4bit_cpu_with_fsm (
 
     //signals for converting
     wire rst = ! rst_n;
-    wire in_data;
-    wire in_addr;
-    wire in_opcode;
-    wire out_data;
+    wire [3:0] in_data = in_data_eightBit[7:4];
+    wire [3:0] in_addr = in_addr_eightBit[7:4];
+    wire [3:0] in_opcode = in_opcode_eightBit[7:4];
+    reg [3:0] out_data;
 
     //declaration register for accumulator, memory, Flip_Flops for write-enabling and for FSM-state
     reg [3:0] accumulator;	//accumulator
+    reg [3:0] next_accumulator;
     reg [3:0] memory [0:15];	//Storage -array of registers
+    reg [3:0] next_memory [0:15];
     reg write_enable_ff;	//Flip-Flop for write-enabling
-    reg [2:0] fsm_state;	//state of FSM
-    reg [2:0] next_fsm_state;	//next state of FSM
     reg [3:0] operand_a;	//operand A for ALU
+    reg [3:0] next_operand_a;
     reg [3:0] operand_b;	//operand B for ALU
+    reg [3:0] next_operand_b;
+    
+    reg [2:0]fsm_state, next_fsm_state;
+    localparam IDLE	= 3'b000;
+    localparam LOAD	= 3'b001;
+    localparam STORE	= 3'b010;
+    localparam ADD_SUB	= 3'b011;
+    localparam LOGIC	= 3'b100;
+    localparam SHIFT	= 3'b101;
 
     //definition of 'CPUState' for FSM-states, each state is represented by a 3-bit-code
-    typedef enum logic [2:0] {
+    /*typedef enum logic [2:0] {
 	    IDLE,	//state for command execution
 	    LOAD,	//state for LOAD operation
 	    STORE,	//state for STORE operation
 	    ADD_SUB,	//state for ADD or SUB operation
 	    LOGIC,	//state for logic operations (AND, OR, XOR, NOT)
 	    SHIFT	//state for shift operations (shift left, shift right)
-	  } CPUState;
+	  } fsm_state, next_fsm_state;*/
 
-    EightToFourBitConverter mod_inst1(
+    /*EightToFourBitConverter mod_inst1(
 	    .eightBitInput(in_data_eightBit),
 	    .fourBitOutput(in_data),
 	    .clk(clk),
@@ -101,37 +101,45 @@ module tt_um_4bit_cpu_with_fsm (
 	    .eightBitOutput(out_data_eightBit),
 	    .clk(clk),
 	    .rst(rst)
-    );
+    );*/
 
     //Regproc
     always @(posedge clk or posedge rst) begin
 	    if (rst) begin
 		    accumulator <= 4'b0000;
 		    write_enable_ff <= 1'b0;
-		    fsm-state <= IDLE;
-		    memory <= 16'h0000;
+		    fsm_state <= IDLE;
+		    for (integer i=0 ; i<15 ; i = i+1)
+		    begin
+			    memory[i] <= 4'b0000;
+		    end;
 	    end else begin
 		    write_enable_ff <= write_ena;
 		    fsm_state <= next_fsm_state;
 		    operand_a <= next_operand_a;
 		    operand_b <= next_operand_b;
 		    accumulator <= next_accumulator;
+		    for (integer i=0; i<15; i = i+1)
+		    begin
+			    memory[i] <= next_memory[i];
+		    end;
+		    out_data <= accumulator;
 	    end;
     end;
 
     //FSM Logik
     always @(posedge clk) begin
-	    case(fsm-state)
-		    IDLE:	next_fsm_state <=	(opcode == 4'b0011) ? LOAD	:
-			    				(opcode == 4'b0010) ? STORE	:
-							(opcode == 4'b0000 || opcode == 4'b0001) ? ADD_SUB:
-							(opcode == 4'b0100 || opcode == 4'b0101 || opcode == 4'b0110 || opcode == 4'b0111) ? LOGIC:
-							(opcode == 4'b1000 || opcode == 4'b1001) ? SHIFT  :
+	    case(fsm_state)
+		    IDLE:	next_fsm_state <=	(in_opcode == 4'b0011) ? LOAD	:
+			    				(in_opcode == 4'b0010) ? STORE	:
+							(in_opcode == 4'b0000 || in_opcode == 4'b0001) ? ADD_SUB:
+							(in_opcode == 4'b0100 || in_opcode == 4'b0101 || in_opcode == 4'b0110 || in_opcode == 4'b0111) ? LOGIC:
+							(in_opcode == 4'b1000 || in_opcode == 4'b1001) ? SHIFT  :
 							IDLE;
 						
 		    LOAD: 	next_fsm_state <= IDLE;
 		    STORE:	next_fsm_state <= IDLE;
-		    ADD-SUB:	next_fsm_state <= IDLE;
+		    ADD_SUB:	next_fsm_state <= IDLE;
 		    LOGIC:	next_fsm_state <= IDLE;
 		    SHIFT:	next_fsm_state <= IDLE;
 	    endcase;
@@ -140,12 +148,16 @@ module tt_um_4bit_cpu_with_fsm (
     //chose operand with MUX
     always @(posedge clk) begin
 	    case(in_opcode)
-		    4'b0001, 4'b0101, 4'b0110, 4'b0111, 4'b1000, 4'b1001, 4'b1010:
+		    4'b0001, 4'b0101, 4'b0110, 4'b0111, 4'b1000, 4'b1001, 4'b1010: 
+		    begin
 			    next_operand_a <= accumulator;
 		    	    next_operand_b <= in_data;
-		    default:
+		    end
+		    default: 
+		    begin
 			    next_operand_a <= accumulator;
 		    	    next_operand_b <= 4'b0000;
+		    end
 	    endcase;
     end;
 
@@ -153,8 +165,8 @@ module tt_um_4bit_cpu_with_fsm (
     always @(posedge clk) begin
 	    case(fsm_state)
 		    IDLE: next_accumulator <= accumulator;
-		    LOAD: next_accumulator <= memory[addr];
-		    STORE: if (write_ena) next_memory[addr] <= accumulator; //store if writing is enabled
+		    LOAD: next_accumulator <= memory[in_addr];
+		    STORE: if (write_enable_ff) next_memory[in_addr] <= accumulator; //store if writing is enabled
 		    ADD_SUB: next_accumulator <= (in_opcode == 4'b0000) ? operand_a + operand_b: //ADD
 			    		    (in_opcode == 4'b0001) ? operand_a - operand_b: //SUB
 					    accumulator;
@@ -168,10 +180,7 @@ module tt_um_4bit_cpu_with_fsm (
 						accumulator;
 	    endcase;
     end;
-
-    //Output data
-    out_data <= accumulator
-
+    
     // instantiate segment display
     //seg7 seg7(.counter(digit), .segments(led_out));
 
